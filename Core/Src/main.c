f16c03b8c89652ce9ce26e44c2ccc237591956e6
wwxi1 <420345378@qq.com>
2026-08-06 10:52:36 +0800

    将current_led = 1U 改成 2U

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -128,7 +128,7 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-    current_led = 1U; /* 练习点：改成 2U，观察从哪颗 LED 开始 */
+    current_led = 2U; /* 练习点：改成 2U，观察从哪颗 LED 开始 */
 
     /* while 循环：条件成立就反复执行 {} 里的代码 */
     while (current_led <= led_count)
